79469ddb8f6bfdb0ba2ef797e918415eea0940c5
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2022-05-25 17:46:49 -0300

    add person create endpoint

--- go.mod
+++ go.mod
@@ -3,12 +3,24 @@ module test-api
 go 1.17
 
 require (
-	github.com/labstack/echo/v4 v4.7.2 // indirect
+	github.com/ipfs/go-log/v2 v2.5.1
+	github.com/labstack/echo/v4 v4.7.2
+	gorm.io/driver/sqlite v1.3.2
+	gorm.io/gorm v1.23.5
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
--- server.go
+++ server.go
@@ -1,17 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func handleIndex(c echo.Context) error {
+type Person struct {
+	ID        uint   `gorm:"primarykey" json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       uint   `json:"age"`
+}
+
+type API struct {
+	DB *gorm.DB
+}
+
+func (a *API) handleIndex(c echo.Context) error {
 	return c.JSON(http.StatusOK, "API v2 Operational")
 }
 
+func (a *API) handlePersonCreate(c echo.Context) error {
+	newPerson := new(Person)
+	err := json.NewDecoder(c.Request().Body).Decode(&newPerson)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	a.DB.Save(&newPerson)
+
+	return c.JSON(http.StatusCreated, newPerson)
+}
+
+func createDatabase() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("people.db"), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	db.AutoMigrate(&Person{})
+
+	return db, nil
+}
+
 func main() {
+	var log = logging.Logger("api")
+	var api API
+	var err error
+
+	api.DB, err = createDatabase()
+	if err != nil {
+		log.Fatal("could not create or open database: %s", err)
+	}
+
 	e := echo.New()
-	e.GET("/", handleIndex)
+	e.GET("/", api.handleIndex)
+	e.POST("/person", api.handlePersonCreate)
 	e.Logger.Fatal(e.Start(":8080"))
 }
