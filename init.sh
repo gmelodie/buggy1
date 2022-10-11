5b59b4c6d6b7ae5cddea3f3cf375a4d928052338
Gabriel Cruz <8129788+gmelodie@users.noreply.github.com>
2022-10-11 17:20:28 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,11 +2,13 @@
 
 # init script for interview box
 apt update
-apt install -y golang-go git plocate
+apt install -y golang-go git plocate make
 
 updatedb
+
+go install github.com/swaggo/swag/cmd/swag@latest
+echo 'export PATH=$PATH:$HOME/go/bin' >> /root/.bashrc # not sure if this persists
 
 git clone https://github.com/gmelodie/buggy1 /opt/api-server
 cd /opt/api-server
 go build && mv buggy1 api-server && ./api-server &
-
